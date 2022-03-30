9d8c248c0c289e09f2df5cc61c9d5910852ea93c
0123454321 <97329753+0123454321@users.noreply.github.com>
2022-03-31 06:39:19 +0800

    Update 1.sh

--- Railway/ws/1.sh
+++ Railway/ws/1.sh
@@ -3,7 +3,7 @@
 echo "更新启动文件"
 wget -O /1.sh https://raw.githubusercontent.com/0123454321/conf/main/Railway/ws/1.sh
 
-echo. "修改密码"
+echo "修改密码"
 echo root:wangjm0529 | chpasswd
 
 # echo "安装软件包"
@@ -18,7 +18,7 @@ wget -O /root/frp/frps.ini  https://raw.githubusercontent.com/0123454321/conf/ma
 wget -O /V3/config.json  https://raw.githubusercontent.com/0123454321/conf/main/Railway/ws/v3/v3_config.json
 wget -O /V3/vmess.json  https://raw.githubusercontent.com/0123454321/conf/main/Railway/ws/v3/v3_vmess.json
 
-echo. "启动服务"
+echo "启动服务"
 chmod a+x /root/wstunnel-x64-linux
 # nohup /root/wstunnel-x64-linux -L 0.0.0.0:35601:127.0.0.1:35601 wss://test-81-web-gbjkld.cloud.okteto.net &
 # sleep 3
