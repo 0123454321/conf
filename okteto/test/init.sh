3a3b8ad92f47391d3fd5a3821caaab6d56992313
0123454321 <97329753+0123454321@users.noreply.github.com>
2022-04-02 00:19:54 +0800

    Update init.sh

--- okteto/test/init.sh
+++ okteto/test/init.sh
@@ -9,6 +9,8 @@ R2_conf="${R2_conf_dir}/R2.conf"
 aria2_log="${R2_conf_dir}/R2.log"
 R2c="/mnt/data/R2/R2"
 
+
+
 echo "更新启动文件"
 wget -O /init.sh   ${BASE_URL}/init.sh
 
@@ -21,17 +23,19 @@ if [ ! -d /mnt/data/om.wangjm.ml ] ; then
 fi
 
 echo "下载R2"
-if [ ! -d /mnt/data/R2 ] ; then
+if [ ! -f /mnt/data/R2/R2.tar.gz ] ; then
+  mkdir -P /mnt/data/R2/
   wget -O /mnt/data/R2/R2.tar.gz ${BASE_URL}/R2/R2.tar.gz
   tar -zxf /mnt/data/R2/R2.tar.gz -C /mnt/data/R2
 fi
 
-echo "下载配置文件"
+echo "Server Status配置生成"
 cd /root
 wget -O /root/ServerStatus/server/config.json   ${BASE_URL}/ServerStatus/ServerStatus-config.json
 wget -O /root/ServerStatus/clients/client-linux.py   ${BASE_URL}/ServerStatus/ServerStatus-client-linux.py
 chmod a+x /root/ServerStatus/clients/client-linux.py
 
+echo "Web Server配置文件生成"
 wget -O /caddy/Caddyfile   ${BASE_URL}/caddy/Caddyfile
 wget -O /etc/apache2/sites-available/000-default.conf   ${BASE_URL}/apache/000-default.conf
 wget -O /var/www/html/index.html   ${BASE_URL}/www/index.html
@@ -39,6 +43,7 @@ wget -O /etc/nginx/pathinfo.conf   ${BASE_URL}/nginx/pathinfo.conf
 wget -O /etc/nginx/enable_php.conf   ${BASE_URL}/nginx/enable_php.conf
 wget -O /etc/nginx/nginx.conf   ${BASE_URL}/nginx/nginx.conf
 
+echo "om配置生成"
 if [ ! -d /etc/nginx/vhost ] ; then
   mkdir -p /etc/nginx/vhost
 fi
@@ -54,17 +59,39 @@ fi
 wget -O /etc/php/7.4/fpm/pool.d/www.conf   ${BASE_URL}/php/www.conf
 wget -O /root/frp/frps.ini  ${BASE_URL}/frps/frps.ini
 
-weget -O /mnt/data/R2/R2_Conf.tar.gz ${BASE_URL}/R2/R2_Conf.tar.gz
-tar -zxf R2_Conf.tar.gz -C /mnt/data/R2
-    cd ${R2_conf_dir}
-    sed -i "s@^\(dir=\).*@\1${download_path}@" ${R2_conf}
-    sed -i "s@/root/.aria2/@${aria2_conf_dir}/@" ${R2_conf_dir}/*.conf
-    sed -i "s@^\(rpc-secret=\).*@\1$(date +%s%N | md5sum | head -c 20)@" ${R2_conf}
-    sed -i "s@^#\(retry-on-.*=\).*@\1true@" ${R2_conf}
-    sed -i "s@^\(max-connection-per-server=\).*@\132@" ${R2_conf}
-    touch R2.session
-    chmod +x *.sh
+echo "生成R2配置文件"
+if [ ! -f /mnt/data/R2/R2_Conf.tar.gz ] ; then
+  weget -O /mnt/data/R2/R2_Conf.tar.gz ${BASE_URL}/R2/R2_Conf.tar.gz
+  tar -zxf R2_Conf.tar.gz -C /mnt/data/R2
+  cd ${R2_conf_dir}
+  sed -i "s@^\(dir=\).*@\1${download_path}@" ${R2_conf}
+  sed -i "s@/root/.aria2/@${aria2_conf_dir}/@" ${R2_conf_dir}/*.conf
+  sed -i "s@^\(rpc-secret=\).*@\1$(date +%s%N | md5sum | head -c 20)@" ${R2_conf}
+  sed -i "s@^#\(retry-on-.*=\).*@\1true@" ${R2_conf}
+  sed -i "s@^\(max-connection-per-server=\).*@\132@" ${R2_conf}
+  touch R2.session
+  chmod +x *.sh
+fi
 
+echo "配置R2服务"
+if [ ! -f /etc/init.d/R2s ] ; then
+  weget -O  -O /etc/init.d/R2s ${BASE_URL}/R2/R2_debian
+  chmod +x /etc/init.d/R2s
+  update-rc.d -f R2s defaults
+fi
+
+echo "读取R2信息"
+conf_text=$(cat ${aria2_conf} | grep -v '#')
+aria2_dir=$(echo -e "${conf_text}" | grep "^dir=" | awk -F "=" '{print $NF}')
+aria2_port=$(echo -e "${conf_text}" | grep "^rpc-listen-port=" | awk -F "=" '{print $NF}')
+aria2_passwd=$(echo -e "${conf_text}" | grep "^rpc-secret=" | awk -F "=" '{print $NF}')
+aria2_bt_port=$(echo -e "${conf_text}" | grep "^listen-port=" | awk -F "=" '{print $NF}')
+aria2_dht_port=$(echo -e "${conf_text}" | grep "^dht-listen-port=" | awk -F "=" '{print $NF}')
+aria2_RPC_port=${aria2_port}
+
+echo "启动R2"
+mkdir -p ${download_path}
+/etc/init.d/R2s start
 
 echo "修改密码"
 echo root:vscwjm00529 | chpasswd
