5c663926982c7ae3623fb31b0db190bb3ae9bace
0123454321 <97329753+0123454321@users.noreply.github.com>
2022-04-03 18:49:35 +0800

    Update init.sh

--- okteto/test/init.sh
+++ okteto/test/init.sh
@@ -2,7 +2,6 @@
 
 BASE_URL="https://raw.githubusercontent.com/0123454321/conf/main/okteto/test"
 #export log_number=$(date +%Y%m%d%H%M)
-info_out_file="> /dev/null"
 
 echo "校正时区"
 rm -f /etc/localtime 
@@ -41,29 +40,29 @@ fi
 
 echo "Server Status配置生成"
 cd /root
-wget -O /root/ServerStatus/server/config.json   ${BASE_URL}/ServerStatus/ServerStatus-config.json  ${info_out_file}
-wget -O /root/ServerStatus/clients/client-linux.py   ${BASE_URL}/ServerStatus/ServerStatus-client-linux.py ${info_out_file}
+wget -O /root/ServerStatus/server/config.json   ${BASE_URL}/ServerStatus/ServerStatus-config.json > /dev/null
+wget -O /root/ServerStatus/clients/client-linux.py   ${BASE_URL}/ServerStatus/ServerStatus-client-linux.py > /dev/null
 chmod a+x /root/ServerStatus/clients/client-linux.py
 
 echo "Web Server配置文件生成"
-wget -O /caddy/Caddyfile   ${BASE_URL}/caddy/Caddyfile  ${info_out_file}
-wget -O /etc/apache2/sites-available/000-default.conf   ${BASE_URL}/apache/000-default.conf ${info_out_file}
-wget -O /var/www/html/index.html   ${BASE_URL}/www/index.html  ${info_out_file}
-wget -O /etc/nginx/pathinfo.conf   ${BASE_URL}/nginx/pathinfo.conf  ${info_out_file}
-wget -O /etc/nginx/enable_php.conf   ${BASE_URL}/nginx/enable_php.conf  ${info_out_file}
-wget -O /etc/nginx/nginx.conf   ${BASE_URL}/nginx/nginx.conf  ${info_out_file}
+wget -O /caddy/Caddyfile   ${BASE_URL}/caddy/Caddyfile  > /dev/null
+wget -O /etc/apache2/sites-available/000-default.conf   ${BASE_URL}/apache/000-default.conf > /dev/null
+wget -O /var/www/html/index.html   ${BASE_URL}/www/index.html  > /dev/null
+wget -O /etc/nginx/pathinfo.conf   ${BASE_URL}/nginx/pathinfo.conf  > /dev/null
+wget -O /etc/nginx/enable_php.conf   ${BASE_URL}/nginx/enable_php.conf  > /dev/null
+wget -O /etc/nginx/nginx.conf   ${BASE_URL}/nginx/nginx.conf  > /dev/null
 
 echo "OD-j配置文件生成"
-wget -O /etc/nginx/vhost/od.conf  ${BASE_URL}/nginx/vhost/od.conf > ${info_out_file}
-wget -O /etc/nginx/fcgiwrap.conf https://raw.githubusercontent.com/MICHAEL-888/oneindex-j/cdn/nginx/fcgiwrap.conf  ${info_out_file}
-wget -O /etc/nginx/fcgiwrap-php https://raw.githubusercontent.com/MICHAEL-888/oneindex-j/cdn/nginx/fcgiwrap-php  ${info_out_file}
+wget -O /etc/nginx/vhost/od.conf  ${BASE_URL}/nginx/vhost/od.conf  > /dev/null
+wget -O /etc/nginx/fcgiwrap.conf https://raw.githubusercontent.com/MICHAEL-888/oneindex-j/cdn/nginx/fcgiwrap.conf  > /dev/null
+wget -O /etc/nginx/fcgiwrap-php https://raw.githubusercontent.com/MICHAEL-888/oneindex-j/cdn/nginx/fcgiwrap-php  > /dev/null
 
 echo "om配置生成"
 if [ ! -d /etc/nginx/vhost ] ; then
   mkdir -p /etc/nginx/vhost
 fi
-wget -O /etc/nginx/vhost/om.wangjm.ml.conf    ${BASE_URL}/nginx/vhost/om.wangjm.ml.conf ${info_out_file}
-wget -O /etc/nginx/vhost/reproxy.conf    ${BASE_URL}/nginx/vhost/reproxy.conf ${info_out_file}
+wget -O /etc/nginx/vhost/om.wangjm.ml.conf    ${BASE_URL}/nginx/vhost/om.wangjm.ml.conf > /dev/null
+wget -O /etc/nginx/vhost/reproxy.conf    ${BASE_URL}/nginx/vhost/reproxy.conf > /dev/null
 if [ ! -f /etc//mnt/data/log/om.wangjm.ml.log ] ; then
   mkdir -p /mnt/data/log
   touch /mnt/data/log/om.wangjm.ml.log
@@ -72,8 +71,8 @@ if [ ! -f /etc//mnt/data/log/om.wangjm.ml.log ] ; then
 fi
 
 echo "PHP配置生成"
-wget -O /etc/php/7.4/fpm/pool.d/www.conf   ${BASE_URL}/php/www.conf ${info_out_file}
-wget -O /root/frp/frps.ini  ${BASE_URL}/frps/frps.ini ${info_out_file}
+wget -O /etc/php/7.4/fpm/pool.d/www.conf   ${BASE_URL}/php/www.conf > /dev/null
+wget -O /root/frp/frps.ini  ${BASE_URL}/frps/frps.ini > /dev/null
 
 
 echo "修改密码"
