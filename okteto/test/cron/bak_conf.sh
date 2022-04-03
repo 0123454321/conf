c06bf332744f462ac70134619bf828065c412172
0123454321 <97329753+0123454321@users.noreply.github.com>
2022-04-03 21:00:48 +0800

    Update bak_conf.sh

--- okteto/test/cron/bak_conf.sh
+++ okteto/test/cron/bak_conf.sh
@@ -1,1 +1,3 @@
-0 */2 * * *  php /mnt/data/oneindex-j/one.php upload:file  /mnt/data/cr/cloudreve.db  /BAK_SERVER/Okteto/CR/cloudreve_$(date +%Y%m%d%H%M).db
+#!/bin/bash
+
+ php /mnt/data/oneindex-j/one.php upload:file  /mnt/data/cr/cloudreve.db  /BAK_SERVER/Okteto/CR/cloudreve_$(date +%Y%m%d%H%M).db
