aba168ba08cbb5e6156023a92def7f218df5ef13
0123454321 <97329753+0123454321@users.noreply.github.com>
2022-08-07 01:21:21 +0800

    Update start.sh

--- okteto/URL_start/start.sh
+++ okteto/URL_start/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -Lso- https://raw.githubusercontent.com/0123454321/conf/main/okteto/URL_start/init.sh
+curl  https://raw.githubusercontent.com/0123454321/conf/main/okteto/URL_start/init.sh | bash
