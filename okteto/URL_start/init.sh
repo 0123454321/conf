8049326a74f7e0fd1d5a2ca2b3bd4cdd32975d50
0123454321 <97329753+0123454321@users.noreply.github.com>
2022-08-07 01:37:28 +0800

    Update init.sh

--- okteto/URL_start/init.sh
+++ okteto/URL_start/init.sh
@@ -3,6 +3,6 @@
 echo "--------------------------------TEST"
 echo "222222"
 apt install iputils-ping -y
-echo "******************33333333333"
-ping www.baidu.com
+echo "***11111111111***************33333333333"
+ping www.google.com
 bash
